f3dc2836dafd2b94dcdf4f8581042d712b248832
Prakkks <tprakrity@gmail.com>
2024-05-13 22:02:18 +0545

    Added templates

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
--- playground/views.py
+++ playground/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here. 
 
+
+def calculate():
+    x=1
+    y=2
+    return x * y
+
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x=calculate()
+    return render(request, 'hello.html', {'name': x} )
